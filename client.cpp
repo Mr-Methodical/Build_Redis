f81d4166f1ad8b53b46b62d0cdc5ec00d81be29a
Ryan Howe <r3howe@uwaterloo.ca>
2026-08-08 00:04:32 -0400

    testing the event loop with a bigger message, 32 MB, so I had to allocate on heap instead of stack. We also split up the query function so that we could see the pipelined requests

--- client.cpp
+++ client.cpp
@@ -6,8 +6,11 @@
 #include <unistd.h> // for write
 #include <cassert>
 #include <errno.h>
+#include <string>
+#include <vector>
 
-const size_t k_max_msg = 4096;
+const size_t k_max_msg = 32 << 20; // 32 Megabytes, probably larger than
+// the kernel buffer
 
 static void msg(const char *msg) {
     fprintf(stderr, "%s\n", msg);
@@ -55,43 +58,49 @@ static int32_t write_all(int fd, const char *buf, size_t n) {
     return 0;
 }
 
-static int32_t query(int fd, const char *text) {
-    // we are going to query the server
-    uint32_t len = (uint32_t)strlen(text);
+static int32_t send_req(int fd, const char *text, size_t len) {
     if (len > k_max_msg) {
         return -1; // not possible
     }
     // send request:
-    char wbuf[4 + k_max_msg];
-    memcpy(wbuf, &len, 4); // we are still assuming little endian here
-    memcpy(&wbuf[4], text, len);
-    if (int32_t err = write_all(fd, wbuf, 4 + len)) {
+    std::vector<char> wbuf(4 + len);
+    uint32_t len_32 = (uint32_t)len;
+    memcpy(wbuf.data(), &len_32, 4); // we are still assuming little endian here
+    memcpy(wbuf.data() + 4, text, len);
+    if (int32_t err = write_all(fd, wbuf.data(), 4 + len)) {
         // failed it didn't give success
         return err;
     }
-    // 4 byte header:
-    char rbuf[4 + k_max_msg];
+    return 0;
+}
+
+static int32_t read_res(int fd) {
     errno = 0;
-    int32_t err = read_full(fd, rbuf, 4);
+    char header[4];
+    int32_t err = read_full(fd, header, 4);
     if (err) {
         // there was some error but we are not sure if it was a good error
         // because hitting end of the file would not trigger it   
         msg(errno == 0 ? "EOF" : "read() error");
         return err;
     }
-    memcpy(&len, rbuf, 4);
+    uint32_t len = 0;
+    memcpy(&len, header, 4);
     if (len > k_max_msg) {
         msg("too long");
         return -1; // to say it is an error
     }
+    // we do this later so we don't waste space
+    std::vector<char> rbuf(len);
     // reply body
-    err = read_full(fd, &rbuf[4], len);
+    err = read_full(fd, rbuf.data(), len);
     if (err) {
         // something other than 0 which is not success
         msg("read() error");
+        return err;
     }
     // we will do something:
-    printf("server says: %.*s\n", len, &rbuf[4]);
+    printf("server says: %.*s\n", len, rbuf.data());
     return 0; // for no issues
 }
 
@@ -111,6 +120,7 @@ struct SocketWrapper {
 };
 
 int main() {
+    // for IPv4 and TCP
     int raw_fd = socket(AF_INET, SOCK_STREAM, 0);
     if (raw_fd < 0) {
         die("socket()");
@@ -127,14 +137,25 @@ int main() {
     if (rv) {
         die("connect()");
     }
-    int32_t err = query(conn.fd, "hello1");
-    if (err) {
-        // note that close(conn.fd) will be autocalled because of RAII
-        return err;
+    std::vector<std::string> query_list = {
+        "hello1", "hello2", "hello3",
+        std::string(k_max_msg, 'z'), // will require many event loop iterations
+        "hello5",
+    };
+    // sending data
+    for (const std::string &s : query_list) {
+        int32_t err = send_req(conn.fd, s.data(), s.size());
+        if (err) { 
+            msg("send_req err");
+            return err;
+        }
     }
-    err = query(conn.fd, "hello2");
-    if (err) {
-        return err;
+    for (size_t i = 0; i < query_list.size(); ++i) {
+        int32_t err = read_res(conn.fd);
+        if (err) {
+            msg("read_res error");
+            return err;
+        }
     }
     return 0;
 }
